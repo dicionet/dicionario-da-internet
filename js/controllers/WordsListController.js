84dbae618953b3f0cb14c3c5d38f30880248dd47
dicionet <gabriel_vianna22@hotmail.com>
2015-06-16 20:25:59 -0300

    Projeto dicionet versao 1.0

---
+++ js/controllers/WordsListController.js
@@ -0,0 +1,365 @@
+angular.module('dicioNet').controller('WordsListController', 
+    function($scope){
+        $scope.words = [
+            {
+                "name": "4Sho",
+                "meaning": "For sure (Com certeza)"
+            },
+             {
+                "name": "Aff",
+                "meaning": "expressão usada para demonstrar insatisfação, discordância"
+            },
+             {
+                "name": "AFK",
+                "meaning": "Away from keyboard (Longe do teclado)"
+            },
+             {
+                "name": "Anon",
+                "meaning": "Anônimo"
+            },
+             {
+                "name": "Attwhore",
+                "meaning": "Pessoa querendo chamar atenção"
+            },
+             {
+                "name": "Baw",
+                "meaning": "Choro"
+            },
+
+
+             {
+                "name": "Bestoftheday",
+                "meaning": "A melhor (postagem ou foto) do dia."
+            },
+             {
+                "name": "Brasil",
+                "meaning": "hashtag que agrupa tweets e fotos de brasileiros"
+            },
+             {
+                "name": "BRB",
+                "meaning": "Be Right Back"
+            },
+
+             {
+                "name": "BTW",
+                "meaning": "By The Way – A propósito"
+            },
+
+             {
+                "name": "Butthurt",
+                "meaning": "– Bunda doída, equivale a “dor de cotovelo”"
+            },
+             
+
+             {
+                "name": "Check out",
+                "meaning": "Cheque Fora "
+            },
+             {
+                "name": "Comment4comm",
+                "meaning": "hashtag usada para troca de comentários"
+            },
+
+            {
+                "name": "CYA",
+                "meaning": "See you"
+            },
+             
+
+             {
+                "name": "DIY",
+                "meaning": "abreviação de Do it yourself, para mostrar a todos que aquele trabalho manual tem um passo-a-passo"
+            },
+             {
+                "name": "Fail",
+                "meaning": "Falha"
+            },
+            {
+                "name": "Fio",
+                "meaning": "Tópico, Thread"
+            },
+            {
+                "name": "Fitness",
+                "meaning": "- Estar em boa forma física; Utilizada principalmente pelos frequentadores de academia."
+            },
+            {
+                "name": "Flood",
+                "meaning": "Inundar, alagar (excesso de informação em pouco tempo)"
+            },
+            {
+                "name": "Flop",
+                "meaning": "- Ferrar, estragar"
+            },
+
+            {
+                "name": "Follow",
+                "meaning": "Seguir"
+            },
+            {
+                "name": "Followback",
+                "meaning": "Sigo de volta"
+            },
+            {
+                "name": "Followme",
+                "meaning": "Hashtag usada para ganhar seguidores"
+            },
+
+            {
+                "name": "FTW",
+                "meaning": "For the win (Para a vitória!) (usado para terminar um argumento)"
+            },
+            {
+                "name": "GTG",
+                "meaning": "Got to go/ Good to know (Tenho que ir/ Bom saber) (Dependendo do contexto)"
+            },
+            {
+                "name": "Hype",
+                "meaning": "Palavra derivada de uma hipérbole, caracteriza um estilo comportamental"
+            },
+
+            {
+                "name": "IDK/Dunno",
+                "meaning": "I don’t know"
+            },
+            {
+                "name": "Igers",
+                "meaning": "Amantes do Instagram."
+            },
+            {
+                "name": "Instadaily",
+                "meaning": "Usado para fotos ou imagens que revelem como é o dia a dia da pessoa."
+            },
+
+            {
+                "name": "instagood",
+                "meaning": "A maioria das hastags no Instagram vêm acompanhadas de “insta”. Essa significa: eu me orgulho dessa foto que tirei."
+            },
+
+            {
+                "name": "Iphoneasia",
+                "meaning": "a hashtag foi criada pelos usuários da Ásia mas se popularizou rapidamente e hoje usuários do mundo inteiro usam a marcação"
+            },
+            {
+                "name": "Iphoneonly",
+                "meaning": "destinado ao iPhone, esse reflete a preferência os usuários do iPhone em relação aos de celulares com android."
+            },
+
+            {
+                "name": "I <3 U",
+                "meaning": "Eu te amo"
+            },
+            {
+                "name": "Inb4",
+                "meaning": "Prevejo"
+            },
+            {
+                "name": "LMAO",
+                "meaning": "Laughing my ass off (Morrendo de rir)"
+            },
+            {
+                "name": "LMK",
+                "meaning": "Let me know (Me informe)"
+            },
+            {
+                "name": "LOL",
+                "meaning": "Laughing Out Loud"
+            },
+
+            {
+                "name": "M2",
+                "meaning": " Me Too"
+            },
+            {
+                "name": "Me",
+                "meaning": "hashtag usada para publicar fotos de você mesmo"
+            },
+            {
+                "name": "Mcm",
+                "meaning": "Meu cara mano, meu cara maromba, meu cara manero."
+            },
+            {
+                "name": "Milgrau",
+                "meaning": "Exagero"
+            },
+            {
+                "name": "Nature",
+                "meaning": "palavra inglesa que significa natureza, agrupa tweets e fotos sobre a mesma"
+            },
+            {
+                "name": "Newfag",
+                "meaning": "Novo membro em certo grupo/fórum"
+            },
+
+            {
+                "name": "Nofilter",
+                "meaning": "quando a foto não possui nenhum filtro de edição"
+            },
+            {
+                "name": "Noob",
+                "meaning": "Novato, especialmente em jogos"
+            },
+            {
+                "name": "NP",
+                "meaning": "Now playing (Tocando agora)"
+            },
+            {
+                "name": "NSFW",
+                "meaning": "Not Safe For Work – Conteúdo impróprio para abrir no trabalho"
+            },
+            
+            {
+                "name": "Oldfag",
+                "meaning": "Membro antigo em certo grupo/fórum"
+            },
+            {
+                "name": "OMG",
+                "meaning": "Oh my God (Ai, meu Deus)"
+            },
+            {
+                "name": "Ood",
+                "meaning": "Fotos que refletem seu humor atual."
+            },
+            {
+                "name": "Ootd",
+                "meaning": "Look do Dia"
+            },
+            {
+                "name": "ORLY",
+                "meaning": "Oh, really"
+            },
+            {
+                "name": "Owned",
+                "meaning": "Humilhado"
+            },
+             {
+                "name": "Pensive",
+                "meaning": "O motivo da postagem é estar envolvido, engajado, ou refletindo em uma causa ou sobre uma reflexão séria."
+            },
+             {
+                "name": "Pica",
+                "meaning": "Picture, Imagem"
+            },
+             {
+                "name": "Picstitch",
+                "meaning": "Quando as pessoas usam o aplicativo Pic Stitch para criar montagens em seu celular."
+            },
+             {
+                "name": "PLZ",
+                "meaning": "Please"
+            },
+            {
+                "name": "Regram",
+                "meaning": "Repostar uma imagem que alguém já tinha postado anteriormente."
+            },
+            {
+                "name": "Riomonumentos",
+                "meaning": "hashtag muito usada por cariocas que registram monumentos pela cidade"
+            },
+            {
+                "name": "ROTF",
+                "meaning": "Rolling on the floor (Rolando no chão) (de tando rir)"
+            },
+            {
+                "name": "SDV",
+                "meaning": "Seguir de volta"
+            },
+            {
+                "name": "Selfie",
+                "meaning": "Auto-retrato."
+            },
+            {
+                "name": "SMH",
+                "meaning": "Shake my head (Sacuda minha cabeça) (usado para relatar algo bizarro)"
+            },
+            {
+                "name": "SNAFU",
+                "meaning": "Situation normal, all fucked up (Situação normal, todos ferrados!)"
+            },
+            {
+                "name": "Soufã",
+                "meaning": "hashtag muito usada por usuários que publicam fotos ou falam de seus ídolos"
+            },
+            {
+                "name": "Spoiler",
+                "meaning": "contar algo que as pessoas ainda não saibam (geralmente é considerado spoiler aquele que conta o final de um filme, livro ou algo do tipo)"
+            },
+            {
+                "name": "SQN",
+                "meaning": "Só Que Não"
+            },
+            {
+                "name": "SRSLY",
+                "meaning": "Seriously (Sério)"
+            },
+            {
+                "name": "Stalkear",
+                "meaning": "pesquisar a fundo sobre algo ou alguém;"
+            },
+            {
+                "name": "SRY",
+                "meaning": "Sorry"
+            },
+             {
+                "name": "Tagsforlikes",
+                "meaning": "A pessoa coloca várias hashtags para conseguir curtidas."
+            },
+             {
+                "name": "Tbt",
+                "meaning": "Throwback Thursday. Significa “voltar no tempo”. É usada para postar fotos de quando uma pessoa era jovem, criança ou bebê."
+            },
+             {
+                "name": "Tcot",
+                "meaning": "Bastante usada no Twitter e significa Top Conservatives on "
+            },
+             {
+                "name": "Twitter",
+                "meaning": "Uma coleção de pensamentos conservadores."
+            },
+             {
+                "name": "TKS/THX/TY",
+                "meaning": "Thanks"
+            },
+             {
+                "name": "TMI",
+                "meaning": "Too much information (Muita informação)"
+            },
+             {
+                "name": "TTYL",
+                "meaning": "Talk to you later (Falo com você depois)"
+            },
+            {
+                "name": "#tweegram",
+                "meaning": "Esse é um famoso aplicativo que permite criar imagens com textos ou citações sem limites de caracteres."
+            },
+            {
+                "name": "W8",
+                "meaning": "Wait"
+            },
+            {
+                "name": "WTH",
+                "meaning": "What The Hell"
+            },
+            {
+                "name": "WTF",
+                "meaning": "What The Fuck?"
+            },
+            {
+                "name": "XOXO",
+                "meaning": "Beijos e abraços"
+            },
+            {
+                "name": "Yep",
+                "meaning": "Yes, Sim"
+            },
+            {
+                "name": "YOLO",
+                "meaning": "You only live once (Você só vive uma vez)"
+            }
+
+        ];
+       $scope.showMe = function(word){
+            $("#wordTitle").empty().append(word.name);
+            $("#wordMeaning").empty().append(word.meaning);
+            $('html, body').animate({ scrollTop: $('#answer').offset().top }, 'slow');
+       };
+    });
